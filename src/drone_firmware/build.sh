009a5366f28f0cb4cf58507d5759de0a3fcbe838
Tyler Nguyen <112019733+nguyent11@users.noreply.github.com>
2025-04-30 15:08:55 -0500

    Create build.sh

---
+++ src/drone_firmware/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This file can be used as a top-level build system. It works by moving the module files into the necessary positions.
+# Then, it calls the build system used by PX4 to include new modules.
